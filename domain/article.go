6c918199e9734de680098a4f3036a2e3f9951203
constraintautomaton <constraintautomaton@protonmail.com>
2022-06-06 15:18:01 -0400

    result page put in place

--- domain/article.go
+++ domain/article.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Article struct {
-	Title    string `json:"title"`
-	Abstract string
-	Url      string
-	Year     int `json:"annee"`
-	DOI      string
-	Author   string `json:"author"`
-	ID       string `json:"idproprio"`
+	Title       string `json:"title"`
+	Url         string
+	Year        int    `json:"annee"`
+	Author      string `json:"author"`
+	ID          string `json:"idproprio"`
+	Journal     string `json:"titrerev"`
+	Text        string `json:"text"`
+	RelatedText string
 }
 
 func (a *Article) BuildUrl() {
@@ -24,12 +25,10 @@ func NewDummyResults(n int) []Article {
 	res := make([]Article, n)
 	for i := 0; i < n; i++ {
 		res[i] = Article{
-			Title:    randomdata.SillyName(),
-			Author:   randomdata.SillyName(),
-			Abstract: randomdata.Paragraph(),
-			Url:      fmt.Sprintf("https://%v.com", randomdata.SillyName()),
-			Year:     randomdata.Number(1900, 2022),
-			DOI:      fmt.Sprintf("https://doi/%v.com", randomdata.SillyName()),
+			Title:  randomdata.SillyName(),
+			Author: randomdata.SillyName(),
+			Url:    fmt.Sprintf("https://%v.com", randomdata.SillyName()),
+			Year:   randomdata.Number(1900, 2022),
 		}
 	}
 	return res
