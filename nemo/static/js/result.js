7a44cdbe7a7c5928aa864f9e6bdb7f95c78c68a9
constraintautomaton <constraintautomaton@protonmail.com>
2022-08-20 00:30:23 -0400

    copy to clipboard added

---
+++ nemo/static/js/result.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const Toastify = window.Toastify;
+
+function copyToClipboard(text_Class) {
+  let text = "";
+  const text_elements = document.getElementsByClassName(text_Class);
+  for (let el of text_elements) {
+    text += el.textContent + ' ';
+  }
+  text = text.substring(0,text.length-1)
+  navigator.clipboard.writeText(text);
+  Toastify({
+    text: "text copier dans le presse papier",
+    duration: 1000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    onClick: function(){} // Callback after click
+  }).showToast();
+}
